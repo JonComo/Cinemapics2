39ba84cf2806ee763574bf097b655d7096f47f12
Jon Como <jon@underground.net>
2013-06-08 23:05:48 -0700

    Initial Commit

---
+++ Cinemapics/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  Cinemapics
+//
+//  Created by Jon Como on 6/8/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cinemapics/CPViewController.m
@@ -0,0 +1,29 @@
+//
+//  CPViewController.m
+//  Cinemapics
+//
+//  Created by Jon Como on 6/8/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "CPViewController.h"
+
+@interface CPViewController ()
+
+@end
+
+@implementation CPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
